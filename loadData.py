c77e81d77597450af65b061cb96319ac16318a7a
Vahan Babushkin <vb1121@nyu.edu>
2021-05-12 03:07:56 +0400

    Add files via upload

---
+++ loadData.py
@@ -0,0 +1,18 @@
+# utility to load the preprocessed EEG data (power spectral densities)
+import pickle
+import numpy as np
+
+def loadData(path, numClasses):
+    # load the data
+    X = []
+    y = []
+    for i in range(numClasses):
+        with open(path+'class'+str(numClasses)+'_'+str(i+1)+'.pickle', 'rb') as handle:
+            tmpX =pickle.load( handle)
+            X.append(tmpX)
+            y.append(i * np.ones((tmpX.shape[3])))
+
+    X = np.concatenate(X,axis=3)
+    y = np.concatenate(y, axis = 0)
+
+    return (X,y)
